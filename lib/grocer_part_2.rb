a8a40064ea106c5db985f32e975b83e4c0f85ae8
Riley Iverson <rmiverson99@gmail.com>
2020-11-12 23:12:29 +0000

    Automatically backed up by Learn

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -5,29 +5,21 @@ require 'pry'
 def apply_coupons(cart, coupons)
   cart.map { |produce|
     produce.map { |key, value|
-      
       coupons.map { |tag|
         tag.map { |t_key, t_value|
-          
           if value == t_value && key == t_key
             produce[:count] = produce[:count] - tag[:num]
-            
             cart.push({
               :item => t_value + " W/COUPON",
               :price => tag[:cost] / tag[:num],
               :clearance => produce[:clearance],
               :count => tag[:num]
             })
-            
           end
-          
         }
       }
-     
-      
     }
   }
-  puts cart
   return cart
 end
 
